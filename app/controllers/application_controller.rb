63ab6099f104a2f80dcb3ac71602094c5366c159
obmax <Oberemko.maxim@gmail.com>
2019-05-16 00:52:34 +0300

    completed cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include CurrentCart
+  before_action :set_cart
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
