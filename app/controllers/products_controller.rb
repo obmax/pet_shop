f4f5c730e25eb74b04554bfb86bbc4a9931e080a
obmax <Oberemko.maxim@gmail.com>
2019-05-16 03:21:55 +0300

    created cart, added products creation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:product_name, :product_brand, :product_description, :price)
+      params.require(:product).permit(:product_name, :product_brand, :product_description, :price, :amount, :image)
     end
 end
